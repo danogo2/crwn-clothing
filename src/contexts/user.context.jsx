9cc4f3516de0f5367e219bd987e1b6642bf9b160
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2023-07-24 14:30:19 +0200

    add reducer to the user context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useReducer } from 'react';
 /* createContext is a way to share data between components without having to pass down props manually at every level of the component tree. 
   Call createContext outside of any components to create a context.
   const SomeContext = createContext(null); //createContext(<default_value>) set default value to null if there isn't any
@@ -21,19 +21,45 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const userReducer = (state, action) => {
+  console.log('dispatched');
+  const { type, payload } = action;
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      throw new Error(`Unhandled type ${type} of action in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
   // UserProvider allows any of its children to have access to its useState values
-  const [currentUser, setCurrentUser] = useState(null);
+  //// const [currentUser, setCurrentUser] = useState(null);
+  // Instead of useState we are using useReducer!!!
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const { currentUser } = state;
+  const setCurrentUser = user => {
+    dispatch({ type: 'SET_CURRENT_USER', payload: user });
+  };
+
   // value that we pass to our Provider that we want all of its children to have access to
   const value = { currentUser, setCurrentUser };
+  console.log('currentUser:', currentUser);
   // sign out when this component mounts. It is needed because our auth singleton remembers that we are logged in even after page refresh
-  // signOutUser(); // doesn't work
+  //// signOutUser(); // doesn't work
   // when this component mounts set auth state change observer, has to be cleared when component unmounts to prevent memory leak
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener(user => {
       // user is a user value provided by onAuthStateChanged
       // this callback is called first when observer is set during the component mount and then on every auth state change
-      console.log(user);
       if (user) {
         createUserDocumentFromAuth(user);
       }
